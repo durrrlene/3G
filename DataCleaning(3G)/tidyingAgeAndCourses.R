e85144e0413dec6f714eb0689e43af03084d5d58
durrrlene <darlene7703@gmail.com>
2024-05-03 15:31:52 +0800

    changed to acro huhu

--- DataCleaning(3G)/tidyingAgeAndCourses.R
+++ DataCleaning(3G)/tidyingAgeAndCourses.R
@@ -41,3 +41,43 @@ View(deets)
 
 
 # ----------------------------------- Changing Courses to its acronym ----------
+
+# replacing long terms to acronyms
+
+# show courses
+print(deets$Course)
+
+# to BSIT
+to_bsit <- c(1, 14, 46, 56, 59, 61, 68)
+deets$Course[to_bsit] <- "BSIT"
+
+# to BSCE
+to_bsce <- c(2)
+deets$Course[to_bsce] <- "BSCE"
+
+#to BS Agri
+to_bsagri <- c(17, 40)
+deets$Course[to_bsagri] <- "BS Agri"
+
+# to BS Chem
+to_bschem <- c(16, 80)
+deets$Course[to_bschem] <- "BS Chem"
+
+# to bsa
+to_bsa <- c(25, 28)
+deets$Course[to_bsa] <- "BSA"
+
+# to bshm
+to_bshm <- c(31, 98) 
+deets$Course[to_bshm] <- "BSHM"
+
+# to bs phar
+to_bsphar <- c(44)
+deets$Course[to_bsphar] <- "BS Phar"
+
+deets$Course
+
+# HAY FINALLY KAPOY BA HAHAHA
+
+
+
