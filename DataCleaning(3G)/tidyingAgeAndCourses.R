640d36d13b3f774cd1ee0118726e2fd5801fd091
durrrlene <darlene7703@gmail.com>
2024-05-06 22:45:12 +0800

    done with the UTAUT

--- DataCleaning(3G)/tidyingAgeAndCourses.R
+++ DataCleaning(3G)/tidyingAgeAndCourses.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(dplyr)
 library(tidyr)
+library(knitr)
 
 
 
@@ -68,7 +69,7 @@ to_bsa <- c(25, 28)
 deets$Course[to_bsa] <- "BSA"
 
 # to bshm
-to_bshm <- c(31, 98) 
+to_bshm <- c(29, 98) 
 deets$Course[to_bshm] <- "BSHM"
 
 # to bs phar
@@ -77,7 +78,178 @@ deets$Course[to_bsphar] <- "BS Phar"
 
 deets$Course
 
+View(deets)
+
 # HAY FINALLY KAPOY BA HAHAHA
 
 
 
+  # ------------------------------------ UTAUT ---------------------------
+
+conversion <- function(response) {
+  if (response == "Strongly Disagree") {
+    return(1)
+  } else if (response == "Disagree") {
+    return(2)
+  } else if (response == "Neutral") {
+    return(3)
+  } else if (response == "Agree") {
+    return(4)
+  } else {
+    return(5)
+  }
+}
+
+
+columns_to_convert <- c(
+  "By using the Adobe application enables you to accomplish tasks more quickly.",
+  "By using the Adobe application it increases my productivity.",
+  "The Adobe application makes work more interesting.",
+  "My peers influence necessary to use the Adobe application.",
+  "People who are important to me think that I should use the Adobe application.",
+  "I have the resources necessary to use the Adobe application.",
+  "My peers is available for assistance with Adobe application's difficulties.",
+  "I could complete a job or task using the Adobe application if  there was no one around to tell me what to do as I go.",
+  "I could complete a job or task using the Adobe application if I had a lot of time to complete the job for which the software was provided.",
+  "I could complete a job or task using the Adobe application if I had just the built-in help facility for assistance."
+)
+
+for (column in columns_to_convert) {
+  deets[[column]] <- sapply(deets[[column]], conversion)
+}
+
+conversion1 <- function(response) {
+  if (response == "Yes") {
+    return(1)
+  } else {
+    return(2)
+  }
+}
+
+columns_to_convert1<- c(
+  "In general, the school has supported the use of the Adobe application",
+  "I know the knowledge necessary to use the Adobe application.",
+  "I intend to use the Adobe application in the next 2 months.",
+  "I predict I will use the Adobe application in the next 2 months.",
+  "I plan to use the system in the next 2 months."
+)
+
+for (column in columns_to_convert1) {
+  deets[[column]] <- sapply(deets[[column]], conversion1)
+}
+
+View(deets)
+
+
+performanceExpectancy <- c(
+  "By using the Adobe application enables you to accomplish tasks more quickly.",
+  "By using the Adobe application it increases my productivity."
+)
+
+# getting the average
+means1 <- colMeans(deets[performanceExpectancy], na.rm = TRUE)
+means1
+
+# getting the standard deviation
+sd_pe <- sapply(deets[performanceExpectancy], sd, na.rm = TRUE)
+sd_pe
+
+
+# calculate the mean and sd for Effort Expectancy
+effortExpectancy <- c(
+  "My interaction with the Adobe application would be clear and understandable.",
+  "It would be easy for me to become more skillful at using the Adobe application.",
+  "Learning to operate the Adobe application is easy for me."
+)
+
+# average
+means2 <- colMeans(deets[effortExpectancy], na.rm = TRUE)
+means2
+
+# standard deviation
+sd_ee <- sapply(deets[effortExpectancy], sd, na.rm = TRUE)
+sd_ee
+
+
+#calculate the mean and sd for Social Influence
+socialInfluence<- c(
+  "My peers influence necessary to use the Adobe application.",
+  "People who are important to me think that I should use the Adobe application."
+)
+
+# average
+means3 <- colMeans(deets[socialInfluence], na.rm = TRUE)
+means3
+
+# standard deviation
+sd_si <- sapply(deets[socialInfluence], sd, na.rm = TRUE)
+sd_si
+
+
+#calculate the mean for Facilitating Conditions
+facilitatingConditions <- c(
+  "I have the resources necessary to use the Adobe application.",
+  "My peers is available for assistance with Adobe application's difficulties.",
+  "I know the knowledge necessary to use the Adobe application."
+)
+
+# average
+means4 <- colMeans(deets[facilitatingConditions], na.rm = TRUE)
+means4
+
+# standard deviation
+sd_fc <- sapply(deets[facilitatingConditions], sd, na.rm = TRUE)
+sd_fc
+
+
+#Calculate the mean for Behavioral Intention to use the system
+behavioralIntention <- c(
+  "I intend to use the Adobe application in the next 2 months.",
+  "I predict I will use the Adobe application in the next 2 months.",
+  "I plan to use the system in the next 2 months."
+)
+
+# average
+means5 <- colMeans(deets[behavioralIntention], na.rm = TRUE)
+means5
+
+# standard deviation
+sd_bi <- sapply(deets[behavioralIntention], sd, na.rm = TRUE)
+sd_bi
+
+
+# combine all factors using kable() function
+pe <- data.frame(
+  Factor = "Performance Expectancy",
+  Mean = colMeans(deets[performanceExpectancy], na.rm = TRUE),
+  SD = sapply(deets[performanceExpectancy], sd, na.rm = TRUE)
+)
+
+ee <- data.frame(
+  Factor = "Effort Expectancy",
+  Mean = colMeans(deets[effortExpectancy], na.rm = TRUE),
+  SD = sapply(deets[effortExpectancy], sd, na.rm = TRUE)
+)
+
+si <- data.frame(
+  Factor = "Social Influence",
+  Mean = colMeans(deets[socialInfluence], na.rm = TRUE),
+  SD = sapply(deets[socialInfluence], sd, na.rm = TRUE)
+)
+
+fc <- data.frame(
+  Factor = "Facilitating Conditions",
+  Mean = colMeans(deets[facilitatingConditions], na.rm = TRUE),
+  SD = sapply(deets[facilitatingConditions], sd, na.rm = TRUE)
+)
+
+bi <- data.frame(
+  Factor = "Behavioral Intention",
+  Mean = colMeans(deets[behavioralIntention], na.rm = TRUE),
+  SD = sapply(deets[behavioralIntention], sd, na.rm = TRUE)
+)
+
+
+data_summary <- rbind(pe, ee, si, fc, bi)
+table <-  kable(data_summary)
+View(table)
