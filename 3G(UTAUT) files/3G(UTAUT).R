66ee96d4d04dec0362e087738cff39180150af03
Lore Faith Sabio <144861299+lorfaith@users.noreply.github.com>
2024-05-07 12:39:56 +0700

    May 07,2024 commit

--- 3G(UTAUT) files/3G(UTAUT).R
+++ 3G(UTAUT) files/3G(UTAUT).R
@@ -58,78 +58,89 @@ performanceExpectancy <- questionnaire[, c(9:11)]
 means1 <- colMeans(performanceExpectancy, na.rm = TRUE)
 sd_pe <- sapply(performanceExpectancy, sd, na.rm = TRUE)
 
+avg_mean1 <- round(mean(means1), 2)
+avg_sd1 <- round(mean(sd_pe), 2)
 
-#calculate the mean and sd for Effort Expectancy
+
+# Calculate the mean and sd for Effort Expectancy
 effortExpectancy <- questionnaire[, c(12:14)]
 
 means2 <- colMeans(effortExpectancy, na.rm = TRUE)
-means2
-
 sd_ee <- sapply(effortExpectancy, sd, na.rm = TRUE)
-sd_ee
 
-#calculate the mean and sd for Social Influence
-socialInfluence<- questionnaire[, c(19:21)]
+avg_mean2 <- round(mean(means2), 2)
+avg_sd2 <- round(mean(sd_ee), 2)
+
+# Calculate the mean and sd for Social Influence
+socialInfluence <- questionnaire[, c(19:21)]
 
 means3 <- colMeans(socialInfluence, na.rm = TRUE)
-means3
-
 sd_si <- sapply(socialInfluence, sd, na.rm = TRUE)
-sd_si
 
-#calculate the mean for Facilitating Conditions
+avg_mean3 <- round(mean(means3), 2)
+avg_sd3 <- round(mean(sd_si), 2)
+
+# Calculate the mean and sd for Facilitating Conditions
 facilitatingConditions <- questionnaire[, c(22:25)]
 
 means4 <- colMeans(facilitatingConditions, na.rm = TRUE)
-means4
-
 sd_fc <- sapply(facilitatingConditions, sd, na.rm = TRUE)
-sd_fc
 
-#Calculate the mean for Behavioral Intention to use the system
+avg_mean4 <- round(mean(means4), 2)
+avg_sd4 <- round(mean(sd_fc), 2)
+
+# Calculate the mean and sd for Behavioral Intention to use the system
 behavioralIntention <- questionnaire[, c(33:35)]
 
 means5 <- colMeans(behavioralIntention, na.rm = TRUE)
-means5
-
 sd_bi <- sapply(behavioralIntention, sd, na.rm = TRUE)
-sd_bi
+
+avg_mean5 <- round(mean(means5), 2)
+avg_sd5 <- round(mean(sd_bi), 2)
 
 #####Make a data frame of all factors######
-item_labels <- colnames(questionnaire[, c(9:11)])
+#item_labels <- colnames(questionnaire[, c(9:11)])
 
 pe <- data.frame(
-  Items = item_labels ,
+  #Items = item_labels,
   Description = "Performance Expectancy",
   Mean = means1,
-  SD = sd_pe
+  SD = sd_pe,
+  Average_Mean = avg_mean1,
+  Average_SD = avg_sd1
 )
-
-View(pe)
 
 ee <- data.frame(
   Description = "Effort Expectancy",
   Mean = means2,
-  SD = sd_ee
+  SD = sd_ee,
+  Average_Mean = total_mean2,
+  Average_SD = total_sd2
 )
 
 
 si <- data.frame(
   Description = "Social Influence",
   Mean = means3,
-  SD = sd_si
+  SD = sd_si,
+  Average_Mean = total_mean3,
+  Average_SD = total_sd3
 )
 
 fc <- data.frame(
   Description = "Facilitating Conditions",
   Mean = means4,
-  SD = sd_fc
+  SD = sd_fc,
+  Average_Mean = total_mean4,
+  Average_SD = total_sd4
 )
 
 bi <- data.frame(
   Description = "Behavioral Intention",
   Mean = means5,
-  SD = sd_bi
+  SD = sd_bi,
+  Average_Mean = total_mean5,
+  Average_SD = total_sd5
 )
 
 #####Combine all factors using kable() function######
@@ -137,7 +148,6 @@ summary <- rbind(pe, ee, si, fc, bi)
 summary_table<-  kable(summary)
 View(summary_table)
 
-View(summary)
 
 
 
